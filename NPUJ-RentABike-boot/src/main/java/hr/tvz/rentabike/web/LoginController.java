bd7a38c750fc7f8386115591de5c280ff1fd049f
vanjs83 <tihomir.vanjurek@gmail.com>
2018-05-15 23:39:20 +0200

    Maknuo nepotrebni import

--- NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/web/LoginController.java
+++ NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/web/LoginController.java
@@ -1,7 +1,5 @@
 package hr.tvz.rentabike.web;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
