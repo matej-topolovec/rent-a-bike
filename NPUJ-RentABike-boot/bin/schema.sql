480740fc287f655a35c622997954b50f42620ea9
vanjs83 <tihomir.vanjurek@gmail.com>
2018-05-16 19:33:13 +0200

    složio dio JdbcRepositoryBike

--- NPUJ-RentABike-boot/bin/schema.sql
+++ NPUJ-RentABike-boot/bin/schema.sql
@@ -1,6 +1,8 @@
 drop table if exists user;
 drop table if exists user_role;
 drop table if exists logging;
+drop table if exists type_bike;
+drop table if exists bike;
 CREATE TABLE user (
 id INT(11) IDENTITY PRIMARY KEY,
 ime VARCHAR(50) NOT NULL,
@@ -20,4 +22,22 @@ id INT(11) IDENTITY PRIMARY KEY,
 username varchar(50),
 action varchar(50),
 action_time timestamp
-)
+);
+
+
+CREATE TABLE type_bike(
+id INT(11) IDENTITY PRIMARY KEY,
+name varchar(50)
+);
+
+CREATE TABLE bike(
+id INT(11) IDENTITY PRIMARY KEY,
+name varchar(50),
+dateAdded timestamp,
+quantity int NOT NULL,
+available int NOT NULL,
+typeid int,
+FOREIGN KEY (typeid) REFERENCES type_bike (id)
+);
+
+
--- NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/web/RentABikeController.java
+++ NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/web/RentABikeController.java
@@ -1,8 +1,6 @@
 package hr.tvz.rentabike.web;
 
 import java.sql.Timestamp;
-import java.time.Instant;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
@@ -11,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import hr.tvz.rentabike.db.JdbcBikeRepository;
 import hr.tvz.rentabike.db.LoggingRepository;
 import hr.tvz.rentabike.model.Logging;
 
@@ -20,10 +19,11 @@ public class RentABikeController {
 	@Autowired
 	LoggingRepository loggingRepository;
 	
+
+	@Autowired
+	JdbcBikeRepository JdbcBikeRepository;
 	
-	
-	@GetMapping("/rentabike")
-	@Secured({"ROLE_USER", "ROLE_DEMO" , "ROLE_ADMIN"})
+	@GetMapping("/home")
 	public String showForm(Model model) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
@@ -40,11 +40,18 @@ public class RentABikeController {
 	
 	
 	@GetMapping("/logging")
+	@Secured({"ROLE_DEMO" , "ROLE_ADMIN"})
 	public String loggingPage(Model model) {
 		model.addAttribute("logging", loggingRepository.findAll());
 		return "logging";
 	}
 	
+	@GetMapping("/rentabike")
+	@Secured({"ROLE_DEMO" , "ROLE_ADMIN"})
+	public String RentABike(Model model) {
+		model.addAttribute("bike", JdbcBikeRepository.findAll());
+		return "bike";
+	}
 	
 	
 }
