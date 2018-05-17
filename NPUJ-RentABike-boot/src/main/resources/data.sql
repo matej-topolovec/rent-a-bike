df02b43f5d8aca095fd32464686a878bcf325fe0
Denis Alibašić <denis.alibasic@ericsson.com>
2018-05-17 12:12:19 +0200

    Create Database/Model[3h]
    -added user and user_role tables
    -added unique constraints on email and username
    -added method for hashing password

--- NPUJ-RentABike-boot/src/main/resources/data.sql
+++ NPUJ-RentABike-boot/src/main/resources/data.sql
@@ -8,6 +8,11 @@ VALUES ('Userko','Userka','user','$2a$10$62iyTepDsDslP/9tp6fQFOov32op4RqDOsmsOFa
 INSERT INTO user_role (username, role)
 VALUES ('user', 'ROLE_USER');
 
+INSERT INTO user(ime, prezime,username,password,enabled)
+VALUES ('Ivo','Ivic','demo','$2a$10$62iyTepDsDslP/9tp6fQFOov32op4RqDOsmsOFaDVv8ZtM2fVd16C', true);
+INSERT INTO user_role (username, role)
+VALUES ('demo', 'ROLE_DEMO');
+
 INSERT INTO logging(username, action, action_time) values ('TestUsername','Test action', '2018-05-15 22:55:19.77');
 
 INSERT INTO type_bike(name) values('Cestovni');
--- NPUJ-RentABike-boot/src/main/resources/schema.sql
+++ NPUJ-RentABike-boot/src/main/resources/schema.sql
@@ -3,20 +3,29 @@ drop table if exists user_role;
 drop table if exists logging;
 drop table if exists type_bike;
 drop table if exists bike;
+
 CREATE TABLE user (
 id INT(11) IDENTITY PRIMARY KEY,
 ime VARCHAR(50) NOT NULL,
 prezime VARCHAR(50) NOT NULL,
 username VARCHAR(45),
+email VARCHAR(45),
+address varchar(50),
+phone int,
 password VARCHAR(100) NOT NULL ,
 enabled TINYINT NOT NULL DEFAULT 1
 );
+
+ALTER TABLE user ADD CONSTRAINT user_username_unique UNIQUE (username);
+ALTER TABLE user ADD CONSTRAINT user_email_unique UNIQUE (email);
+
 CREATE TABLE user_role (
 user_role_id int(11) IDENTITY PRIMARY KEY,
 username varchar(45) NOT NULL,
 role varchar(45) NOT NULL,
 FOREIGN KEY (username) REFERENCES user (username)
 );
+
 CREATE TABLE logging(
 id INT(11) IDENTITY PRIMARY KEY,
 username varchar(50),
