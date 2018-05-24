090f465400d5979c7b5e4824c1fd7ff40d2883e8
vanjs83 <tihomir.vanjurek@gmail.com>
2018-05-24 15:22:21 +0200

    Implement JdbcRepository [3] Add class for JdbcBikeTypeRepository

---
+++ NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/db/BikeTypeRepository.java
@@ -0,0 +1,14 @@
+package hr.tvz.rentabike.db;
+
+import hr.tvz.rentabike.model.BikeType;
+
+public interface BikeTypeRepository {
+	
+
+	Iterable<BikeType> findAll();
+	
+	BikeType findOne(String id);
+	
+	BikeType save(BikeType biketype);
+
+}
---
+++ NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/db/JdbcBikeTypeRepository.java
@@ -0,0 +1,77 @@
+package hr.tvz.rentabike.db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+import hr.tvz.rentabike.model.BikeType;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+
+@Repository
+public class JdbcBikeTypeRepository implements BikeTypeRepository {
+
+	private final String FIND_ALL_QUERY = "select id, name from type_bike";
+	
+	private JdbcTemplate jdbc;
+	private SimpleJdbcInsert bikeTypeInserter;
+	
+	
+	@Autowired
+	public JdbcBikeTypeRepository(JdbcTemplate jdbc) {
+		this.jdbc = jdbc;
+		this.bikeTypeInserter = new SimpleJdbcInsert(jdbc)
+			.withTableName("bike_type")
+			.usingGeneratedKeyColumns("id");
+	}
+	
+	
+	
+	@Override
+	public Iterable<BikeType> findAll() {
+	
+	return jdbc.query(FIND_ALL_QUERY, this::mapRowToBikeType);
+		
+	}
+
+	@Override
+	public BikeType findOne(String id) {
+	
+		return jdbc.queryForObject(FIND_ALL_QUERY + " where id = ?", this::mapRowToBikeType, id);
+		
+	}
+
+	
+	@Override
+	public BikeType save(BikeType bike) {
+		
+		bike.setId(saveBikeTypeDetails(bike));	
+		return bike;
+			
+	}
+
+	
+	private int saveBikeTypeDetails(BikeType bike) {
+		Map<String, Object> values = new HashMap<>();
+		values.put( "name", bike.getName());
+		return bikeTypeInserter.executeAndReturnKey(values).intValue();
+	}
+	
+	
+	
+	
+	 
+	private BikeType mapRowToBikeType(ResultSet rs, int rowNum) throws SQLException{
+	    BikeType bike = new BikeType();
+		bike.setId(rs.getInt("Id"));
+		bike.setName(rs.getString("name"));
+		
+		return bike;
+	}
+	
+}
