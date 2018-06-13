093a52aee08cae79609799610437b7a227c1fe4c
Denis Alibašić <denis.alibasic@ericsson.com>
2018-06-13 10:25:16 +0200

    Konfiguracija za locale
    -dodana konfiguracija za locale i dodan
    novi properties file

--- NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/RentABikeApplication.java
+++ NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/RentABikeApplication.java
@@ -4,21 +4,61 @@ package hr.tvz.rentabike;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 
 
 @SpringBootApplication(exclude = {HibernateJpaAutoConfiguration.class })
 @EnableGlobalMethodSecurity(securedEnabled= true)
-public class RentABikeApplication {
+public class RentABikeApplication extends WebMvcConfigurerAdapter {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(RentABikeApplication.class, args);
 	}
 	
 	
+	@Bean
+	public LocaleResolver localeResolver() {
+	    return new CookieLocaleResolver();
+	}
 
-	
 
+
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	    lci.setParamName("lang");
+	    return lci;
+	}
+
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(localeChangeInterceptor());
+	}
 	
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource= new
+				ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+	return messageSource;
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		LocalValidatorFactoryBean bean= new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+	return bean;
+	}
 }
