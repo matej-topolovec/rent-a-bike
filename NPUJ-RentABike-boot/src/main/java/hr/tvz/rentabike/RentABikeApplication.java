8ddcd79398051f063f99c10e68e225ff9f2bf3ee
Denis Alibašić <denis.alibasic@ericsson.com>
2018-06-13 10:32:03 +0200

    Konfiguracija za locale
    -dodane promjenjene neke funkcije

--- NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/RentABikeApplication.java
+++ NPUJ-RentABike-boot/src/main/java/hr/tvz/rentabike/RentABikeApplication.java
@@ -1,6 +1,8 @@
 package hr.tvz.rentabike;
 
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
@@ -14,6 +16,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 
 
@@ -25,25 +28,24 @@ public class RentABikeApplication extends WebMvcConfigurerAdapter {
 		SpringApplication.run(RentABikeApplication.class, args);
 	}
 	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(localeChangeInterceptor());
+	}
 	
 	@Bean
 	public LocaleResolver localeResolver() {
-	    return new CookieLocaleResolver();
+		SessionLocaleResolver localeResolver= new SessionLocaleResolver();
+		Locale locale= new Locale("hr");
+		localeResolver.setDefaultLocale(locale);
+	return localeResolver;
 	}
-
-
 
 	@Bean
 	public LocaleChangeInterceptor localeChangeInterceptor() {
 	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
 	    lci.setParamName("lang");
 	    return lci;
-	}
-
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-	    registry.addInterceptor(localeChangeInterceptor());
 	}
 	
 	@Bean
